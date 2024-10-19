c62c941bc3b9ae79a2ccb9ad1d85f059419279aa
zeulan <solanhomestead@gmail.com>
2024-10-19 16:17:23 -0400

    df

--- spotify-proccess.py
+++ spotify-proccess.py
@@ -5,7 +5,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # This allows requests from any domain
+CORS(app, resources={r"/*": {"origins": "https://solanh.github.io"}})
 app.secret_key = os.getenv('SECRET_KEY')
 load_dotenv()
 
