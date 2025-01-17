2a6ec3816a595b4168cc028413e6dfdd653a9cab
zeulan <solanhomestead@gmail.com>
2025-01-17 13:28:54 -0500

    much much better

--- tester.py
+++ tester.py
@@ -227,6 +227,23 @@ def check_liked_songs():
     except Exception as e:
         return f"Error retriving songs: {str(e)}", 400
     
+    
+@app.route('/clear_songs_from_playlist', methods=['GET'])
+def clear_songs_from_playlist():
+    sp = spotipy.Spotify(auth=get_valid_token())
+    
+    try:
+        playlist_id = '41Zy0SgcV5GVQ60rB4X4Tl'
+        tracks = sp.playlist_tracks(playlist_id=playlist_id)
+        track_ids = [track['track']['id'] for track in tracks['items']]
+        
+        for i in track_ids:
+            sp.playlist_remove_all_occurrences_of_items(playlist_id=playlist_id, items=i)
+            
+            
+        return "Songs removed from playlist"
+    except Exception as e:
+        return f"Error clearing songs from playlist: {str(e)}", 400
 
 @app.route('/add_songs_to_playlist', methods=['GET'])
 def add_songs_to_playlist():
@@ -239,13 +256,32 @@ def add_songs_to_playlist():
         track_ids = ["0hy3f5LxZjH95QbuysY5OO","4lV1tx0EdlIChOw7Kz7fP0","4sJLWVtLug7LTsuFNqt82k","2e9sPx5xxJFyCecgNpq3Zb","07emIvmVExzrUNRraeC3JJ","6Mxqj3ClyindysAxYqqrrg","1VjyWxlrbNPF4ZtHZiw0Tp","6BSstu7y08lkJT5dNlEOkN","5bCxQhumlHXRjfyjOS2ib6","3tcaltthSz9s6awB6koVRo","6GXlXAfXR7C6u1VjR3VMsm","1RIsAtnYOlo8zGMycNFioq","0ducHx1R45CnEloZ6tUVuC","1uBMgVAUYLnzVL5uQT88FP","3Qjm0halGOZZGIS1tNaXlI","3I7a9joX0lJnK9XzE38GnD","2uuMXKKmdXrY2XxsqTsGp0","3Wunwn44wcWRNB4zb03AvA","7HQrFPtLEpgTJaEVujH8OO","3nsUVlntPIksDvhHdZZhiu","3BGwxsmbbOe2jmZfh5CUAO","12wlYeErSUNGg1B5d64077","6gQOsok1o9YY6vU1W5ZzWX","35XlkvHy9WHPI4Tf9eax4t","5iNr7y6iWNkwVvVeOXSLT5","55ZL7fjGAWfClmpnsK6Xon","6mkBbzyfK0EpywDk17taBg","5kxqPQ5Gasw1jXyBDCPDkN","5v2vkoTamoisazJFanHJjk","4KF1kzyeyPrgafyVm2UgQg","4wCMKxkE3eMOTtlAx0NFEM","7uBnSdqm07uMBrp7nBmMu9","5UdShl12E6eHEgS8lQ1bPr","2UBUvScBA21UPHoLSIEz3V","0VGmxQSqrFFEvZLC1HDGcr","7sYOXMXgA34jo9WkXZLEOG","4lVjHdUBgQhdwOfHStticK","6pS0PVQYaBYGkKPRxT5PEl","4z0YPAr8kcoHrJXhrJsFV3","4McullpiOd45TwEHlOISgs","70i1XQrudvwC4M4qKs9tpA","5dAoUZkkkmx0YDMK9Bmy2g","2ireIPn0SKV7BXXvZ3zdRa","0mE46SXrCohWNSk17AVfbP","6jutYLUIuEUYZnVHflVjUK","7LtSEY5R0vTmFmJxWpH0Kc","2pBobrYuBAVFKhvORvYFee","7Cao2G1t2BbYbSo6YjS4cx","3bntzrqfCdrTjJxRhbf7rs","1OpkNblM40C0Ol0fxldcm3","2nUyMD5cvEwiB6EuLFyCHo","7fyqFZSv5FKmPcx4LhmTPm","51PvMspXXFjLWUfePpeImt","3p9GbXMy1f9Efhdfj0QNBG","4F0GyDm11VAjqQmj7mRYPI","7F8Oirc8pVBeXQ9NFerUXs","43GalYZWoUi9cciYdv0Bej","2gQsVggoV49YeMdoYW03tJ","50M69sNDibJQfgDYNqFfH2","70B5oP5ng1QYClkJ9CeWly","2YGFDRqaOnejsnTkKe4VKE","7dhM0KUBxuZV9z5iNodLyn","2XVGfOXvRWy5QP2xPGXvZj","6KZaXNcnsHYWLT9b2Rdy6H","1EjQRTG53jsinzk2xlVVJP","0C6EIiQu8CS4eYtOCMEiAd","4pxzJlQsVjvjrtbTsey5vX","6BnyhvqA12zhsmcvoqwtz4","6u3hSyz0KAuZMyE4JtbQNp","1QpwjJmuow6GhUJGw6W74P","0IM5GMjsngXb6ub7BXkOnj","3TliaLRGyaZzrw3ffYaNBl","1mouSqtUHU3FtBmEODBqAn","4adskxvtdhBhsq3onGFWXQ","4ccbmgUx4GHmI2aG8BuYj8","5VBXdpi6QULy8N6WUBqnsS","6ZS9riQEtak8T0FitSjGc7","0xuVIKd6k2sLanHe9WcvNi","5C0WgwzC4jhUtAfziMaPxD","5ojPnZWtC3vBdxBKVhwFDn","1smRZTALo3Hwj3WkeQJe74","0kzfORnpKQE9qQeV0661Ue","0rcHIJUwAV7Ea9KxCj1atD","5TadOUh1LUMMfep93Fyy2L","7Eo8B4PQZqcklwHuHUgmBI","0Z4TBn1tde0ta3GAT8oUid","3qhprSKMkxBTvsBDO95ZHy","1yIWIPQ5hKJ7USdTdQIfpp","1ei3OFzWuOLMozHdsiNTou","7CbZjHN8wjfWRkUEXFiFGO","3ED4zmmCHDrFD5LKd8RaEK","0BeXtvkMm4tkAdXBBq9Slc","4Es8LLtaEX0qwRUoQ3vUKw","5gmQKRTzICBzlASai8hA2B","0ddItdjSTDsy0McT0fgug3","0o0qByHcXfGtmZmmkK7pcD","7r4xZTeYE78B1bbc9Gwwnk","01jcX2gEJYIWCxnsiOeYgD","6Xx77z1Z67f6bKXlj6z342","7sw5lEyG3Q2b1X19b7ulxG","2u4fnzW3mQ0FwXkZpsD2o8","4uBwHRHe1EhWwyCXCIljhH","1Pvc8QFH4oxCersJ7GaH8O","1SZO4SCfBSFhTGMlUpZB1O","1hDwBeBz3G5xVYftAjK2BQ","2KMLGJ1mPfRE4GNdL92rl3","1Ny6vWtIlFvPMFLZzUjrlB","6BTTmqavOi1KKpMBMRiYZ8","7euHyiqapZIQ08A4lerMOK","6eoXV1fjEBB9ussPSftEwA","5ifi01PWSK7Nx1FbIZ0jT8","0um7RSSiq1YiFkkYheLVuS","7gu99i0zbE2SIKzXkKW8ZA","4U6gIusvEnrkrSqJDQxCKA","52ifLFAEeFXYfQWMHe0X7y","6KZWM62G22AAOATjRKkHs9","4Q4OPuuuCJ4KHQ5EJvGLIz","0AYPWlZmdBqukG0OOp2Ine","2PPh5d93KFv70OUaUcqDtb","28eSkxyYbmE89HIeZTMnsC","4n3axXPWHMpFBMGZ4GMhgM","1iRG2KAY5bVz7HncTQF14h","3HHc8Vff5i1ogMlXHEoogb","2lL0L2vPfIZ9xdxpEJiQ3S","6Sorz09TdlUnyo2Ja4uNzb","4hD8Y7hZh8btoxflDyLugZ","3fcGcamR01XVSsV44qbNke","7LjkYjpf2uURgrZoCi5KOj","4UGOaZPk8uBm74Pc7hueQi","6pktnzWEkli12iDo6Y1jTz","3hyZYDPhYpZx3IfrrlCEiV","7JY0CTHiOogxhmJBrTBcp0","47cmrLpx1evFKWVsgdLqnX","1tJDnftYNFy9KgDfW0V3Pl","1xYKGXZF1R1AmIO9sayi1b","6BF1zkgQllvTwu5LSl27MU","4qqw1sObbcCIbkg00armxj","1lE1kfzgoTKpWYYADYrwIe","6sdaGuPCEW4kOtOelRxZjX","3Lp49Ke7d6pjz7XTVMXwzV","6qqsCLb53W6dzqijbzAUrn","7CbQNZFLfSk4g0IcjhZRXi","0UHN6K1BofQn0yciyilJGT","41yC7uY7DcptdwNx1afHtv","6XdxafgMPNj7sPtDQPijPo","63oM1GngVrACWQdFdaWeSE","6kDxBKwbpmfzxbF9j9kFSk","5AbLAP2HPoEsK99RXsNL1a","6t7PLCmMFrAXkf1UCwpQz7","43fnC9EwpHeyHNPXXAxq71","5pDx2xXt8Bxu1KawK5X2IH","77HfdYVU9F9RiRJSeKPKPJ","4ppMrMWZB3RPJwaFp2DNm3","6yznYy0ivwdpMVXQ9vbN0d","1XVwLqT5sFiwYxpNzq4RSB","6DabwaBU8Q0b5EG6YndySK","0vCsCgU9SgshVemF34OWwf","2Um6hfUdtXjYf5tHPBND6w","0AuKj2m1pbLk6JABCGmGkD","56IWAEGoeIhChibtncKOin","39TvIkd6vc7wPnGHCGIYmW","0K1SIWZFYeuteaFPn3sCPe","7LUOeqUkDBn5QEeSJZqOaq","2o1GalnDpsjABbU9fTEKQI","2X1EwEwZnwDiNcLk6hVI8W","5zZHskZUtCrvOTkPAECCiU","0SxD0MB6a7gvonrRUIqcwT","7oRNJJs5xn8elH8cb2Nhf4","6CvPC47gpqTznkWt6FY2RO","0kDVug5XXdD1mAOVz7P3El","0B4DXcYatKb19k25NT3p9S","4zULslnXBKMWVOJbvaUKWi","1vXZuXKnTyoDBAJOCtjJFq","0trTpNYmHAK3EihaZBd2h6","4T1168WOWj7Ch7DbbNEFp7","4Dp1NwpExFHNlSjO5jc7zx","3PuS7Y8gmF2mAOTbhJU9lg","5iJ27E0IOYs9eaFLFJwxR6","3vRYYiBghmEKMOnZcKj7MB","2Hxo0qwge0a4DS0Bj2J2hw","49q4JJ9PNjYoZFz2h6oLFY","1fjZGlwC7sBt3qx3fQjQfM","4Hxcdj7lXiOKY1SSBkrap5","2qQNgNregevtyWNX7OiUKI","739wZZ4Qqtm0pnzVHzeKxo","5zHzofWXEpq1KiYIqTv7s2","1ZCDInnCGU2rRTfEhrJaSS","2M7iasCe4t2UgOHVrVK6TM","1iwbnSrCMXUGXY6a6mjTTL","1SC86EEp5h0feV89TI5ECv","4flgLEXHoaXSQ2R8YZkSIe","0fe7AHSTWRryNOYCTci2LE","103XkIUGvwnefUIqDv5XNM","0lHSJ0ZP8uUPnJXhMdGjOK","0mmaX70fKuhgg0hDrfsS29","0lP4HYLmvowOKdsQ7CVkuq","7sMSEO2Xm4qa36nNq43JVE","4kRf54PG35X6wkYHqFUsbl","3UobpTI1cP54bPL1l3l2jP","1JBa9B1iA4yNVpJ5wVGrZH","6xKos15gJxZ9J3Q9qnSUU5","7BvLTfOGjYhxwbAA5CbMJ7","7hZRh76Rl1Tc99jZseanbZ","5XApNGBfWN8qXlhmPhErQb","6UScyVeYTENNMPgkkCYWHg","0DnYvbdoVg88EF2ddh7Tn6","6PY49R5SjxYDiOMFQj7Gsq","0i7AGJG9DX0vI1w798IVKZ","62jc4VA6WPoANaL9Duu8db","3tcMadg7fSfH0mFDo0408i","3WXhshrs1fzwF3rQE399Gq","6kh5fj610DhNF1e02og1PL","6NEyZ1rdMgO90cZ7U4UjO5","7ssKQDHJe7PhzFt0RBFlgt","7eXFZCiP9GqdMUashk96UF","7v9t1j6m5nYYGG1etmAD12","1QxbBB80IuPwhwW1ygGfPh","5R8T6F32PjJflb58Y564G0","01dQHUYz6EerSF6JQDolCC"]
         
         
+        temp_playlist_id = '41Zy0SgcV5GVQ60rB4X4Tl'
         for i in track_ids:
-            sp.playlist_add_items(playlist_id='3sK5fT8l5L7WQr3Pv6v9zT', items=[i])
+            sp.playlist_add_items(playlist_id=temp_playlist_id, items=[i])
+            print("something")
+            
+        return "Songs added to playlist"
 
         
         
     except Exception as e:
         return f"Error adding songs to playlist: {str(e)}", 400
+    
+    
+@app.route('/get_playlist_id', methods=['GET'])
+def get_playlist_id():
+    
+    sp = spotipy.Spotify(auth=get_valid_token())
+    
+    try:
+        playlists = sp.current_user_playlists()
+        for playlist in playlists['items']:
+            if playlist['name'] == 'My New Flask Playlist':
+                return playlist['id']
+        return None
+    except Exception as e:
+        return f"Error retrieving playlist ID: {str(e)}", 400
 
 @app.route('/main', methods=['GET'])
 def main():
